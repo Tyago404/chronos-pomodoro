28a31dfcb6e9f3d1aafa3f2967e0b5c08ffaee91
Tiago Santos <tiago40427@gmail.com>
2025-04-22 22:19:39 -0300

    Components,Styles,Props
    In this commit i learned how to create components, assign styles and Components Properties

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// import Anything from './App'; //Quando você exporta default voce pode importar com qualquer nome
 
+import { App } from './App';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+
+    {/*Só funciona no modo desenvolvimento, serve para identificar problemas no código (é uma boa pratica) */}
     <App />
+
   </StrictMode>,
-)
+);
