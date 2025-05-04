b910a4101caeebd0aa9ac741485c961581d50b70
Tiago Santos <tiago40427@gmail.com>
2025-05-04 17:55:43 -0300

    Adicionando Pastas padrões aos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,21 @@
 //Importanto arquivos css, não precisar ter um nome, importe diretamente o arquivo
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container/Index';
+import { Heading } from './components/Heading/Index';
 
 //Importando Componente
 
 export const App = () => {
   return (
     <>
-      <div className='container'>
-        <div className='content'>
-          <section>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat soluta alias, molestias beatae rerum quas quis distinctio ipsum expedita accusantium! Aliquid necessitatibus voluptatem voluptatibus minima est magnam maiores sunt incidunt!</p>
-          </section>
-        </div>
-      </div>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
 
-      <div className='container-fluid'> {/*Esse container é esticado pela pagina inteira*/}
-        <div className='container'>  {/*Para alinhar o conteudo ao centro e limita-lo */}
-          <div className='content'> {/*Abrigar o conteudo */}
-            <section>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores sit dolor assumenda beatae molestias repellat nulla laborum temporibus excepturi vel. Odit nemo consequuntur sapiente maiores odio quo praesentium vero tempora.</p>
-            </section>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 };
