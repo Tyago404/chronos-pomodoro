ee9cbdf48e2f7ef59fa2a499be3e59118eaf1017
Tiago Santos <tiago40427@gmail.com>
2025-05-13 21:34:08 -0300

    Finalizando Menu
    Neste commit utilizando LocalStorage corrigimos o bug do tema default dark e adicionamos o icone que troca ao mudar o tema

--- src/components/Menu/Index.tsx
+++ src/components/Menu/Index.tsx
@@ -1,55 +1,84 @@
 //Import do css
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react'
-import styles from'./Styles.module.css'
-import { useEffect, useState } from 'react'
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import styles from './Styles.module.css';
+import { useEffect, useState } from 'react';
 
+type AvailableThemes = 'dark' | 'light';
 
-type AvailableThemes = 'dark' | 'light'
+export const Menu = () => {
+  const [theme, setTheme] = useState<AvailableThemes>(()=>{
+    const storageTheme = localStorage.getItem('theme') as AvailableThemes || 'dark'
 
+    return storageTheme
+  });
 
+  const handleThemeChange = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,) => {
+    e.preventDefault();
 
-export const Menu = ()=> { 
-    /////////////////////////
-    // //<dark | light> para que sua variavel aceite apenas os dois estados definidos 
-    //   const [theme, setTheme] = useState<'dark' |'light'>('dark')
-    /////////////////////////
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  };
 
-    //Usando Type
-    const [theme,setTheme] = useState<AvailableThemes>('dark')
-      
-    //Precisamos tipar o evento que o React cria para poder cancelar o mesmo e não seguir o link do href na tag A 
-    const handleThemeChange = (e:React.MouseEvent<HTMLAnchorElement, MouseEvent>)=>{
-        e.preventDefault(); //Desabilita o link da tag A
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    //para criar uma key e o valor da key no localstorage do navegador
+    localStorage.setItem('theme',theme)
+    console.log('Fui Executado! e o theme mudou');
 
-        setTheme((prevTheme)=> {
-          const nextTheme = prevTheme === 'dark'? 'light' : 'dark'
-          return nextTheme
-        })
-      }
-      
-      //Só executa quando o estado de theme mudar
-      useEffect(()=>{
-        document.documentElement.setAttribute('data-theme', theme)
-        console.log('Fui Executado! e o theme mudou')
-        
-        
-      //Retorno de limpeza, executa quando o componente for desmontado. 
-      //Função de clean up
-        return ()=>{
-          console.log('Função de limpeza foi executada!')
-        }
-      },[theme]
-    
-    )
+    return () => {
+      console.log('Função de limpeza foi executada!');
+    };
+  }, [theme]);
 
+  const nextThemeIcon = {
+    dark:  <SunIcon />,
+    light: <MoonIcon />
+  }
 
-  return(
+  return (
     <nav className={styles.menu}>
       <h1>{theme}</h1>
-      <a href="#" className={styles.menuLink} aria-label='ir para home' title='ir para home'> <HouseIcon /></a>
-      <a href="#" className={styles.menuLink} aria-label='ver histórico' title='ver histórico'> <HistoryIcon /></a>
-      <a href="#" className={styles.menuLink} aria-label='ir para configurações' title='ir para configurações'> <SettingsIcon /></a>
-      <a href="#" className={styles.menuLink} aria-label='Mudar tema' title='Mudar tema' onClick={handleThemeChange}> <SunIcon /></a>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='ir para home'
+        title='ir para home'
+      >
+
+        <HouseIcon />
+      </a>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='ver histórico'
+        title='ver histórico'
+      >
+
+        <HistoryIcon />
+      </a>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='ir para configurações'
+        title='ir para configurações'
+      >
+     
+        <SettingsIcon />
+      </a>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Mudar tema'
+        title='Mudar tema'
+        onClick={handleThemeChange}
+      >
+        {nextThemeIcon[theme]}
+      </a>
+
+        
+
     </nav>
-  )
-} 
+  );
+};
