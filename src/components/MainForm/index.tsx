649a6dfe93012b2950b89af4f23dca3d7f2b5a99
Tiago Santos <tiago40427@gmail.com>
2025-05-20 22:21:24 -0300

    Input Controlado
    Neste commit aprendemos criar um input controlado, aonde o REACT intercepta tudo o que está acontecendo dentro do nosso input controlado

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,20 +2,31 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { useState } from "react";
 
 export const MainForm = () => {
 
+  const [taskName, setTaskName] = useState('')
+  const handleCreateNewTask = (e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault()
+    console.log('Deu certo!')
+  }
 
  
   return (
-    <form className='form'>
-
+    //Criar uma função anonima com 'E' como parametro, você passando o mouse em cima consegue descobrir o tipo do evento
+    //exemplo: onSubmit={e=>handleCreateNewTask}... Assim você adquire o React.FormEvent<HTMLFormElement>
+    <form onSubmit={handleCreateNewTask} className='form'>
+        <h1>{taskName}</h1>
       <div className='formRow'>
         <DefaultInput
           type='text'
           id='input'
           labelText='task'
           placeholder='Digite algo'
+          value={taskName}
+          onChange={(e)=>{setTaskName(e.target.value)}}
+
 
           // disabled
           // defaultValue='abc' Testando estilo desabled
