2786c1d6fa2a6f57c4a009fa0459ecebc1aaed9e
Tiago Santos <tiago40427@gmail.com>
2025-05-21 21:57:00 -0300

    Input com o hook useRef

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,35 +2,38 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
-import { useState } from "react";
+import { useRef } from "react";
 
 export const MainForm = () => {
 
-  const [taskName, setTaskName] = useState('')
+ 
+ 
+  //Para criar um Ref, é necessário tipar o elemento que será adquirido a referencia
+  const taskNameInput = useRef<HTMLInputElement>(null)
+
+
   const handleCreateNewTask = (e:React.FormEvent<HTMLFormElement>)=>{
     e.preventDefault()
-    console.log('Deu certo!')
+
+      console.log('Form enviado!')
+      // console.log(taskNameInput.current.value)
+
   }
 
  
   return (
-    //Criar uma função anonima com 'E' como parametro, você passando o mouse em cima consegue descobrir o tipo do evento
-    //exemplo: onSubmit={e=>handleCreateNewTask}... Assim você adquire o React.FormEvent<HTMLFormElement>
+    
     <form onSubmit={handleCreateNewTask} className='form'>
-        <h1>{taskName}</h1>
+     
       <div className='formRow'>
         <DefaultInput
           type='text'
           id='input'
           labelText='task'
           placeholder='Digite algo'
-          value={taskName}
-          onChange={(e)=>{setTaskName(e.target.value)}}
+          ref={taskNameInput}
 
-
-          // disabled
-          // defaultValue='abc' Testando estilo desabled
-          // title='TITLE' Testando ...rest
+         
         />
       </div>
 
