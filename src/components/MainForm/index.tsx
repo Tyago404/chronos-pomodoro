821104d1c6e3fa34023eec0f7743453d44baa89b
Tiago Santos <tiago40427@gmail.com>
2025-05-23 23:32:07 -0300

    Adicionando CurrentCycle
    Neste commit criamos a função que faz a atualização dos cycles de 1 a 8 e utilizamos no setState

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,35 +5,33 @@ import { DefaultInput } from "../DefaultInput";
 import { useRef } from "react";
 import { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
 
 export const MainForm = () => {
 
   const taskNameInput = useRef<HTMLInputElement>(null)
-  const {setState} = useTaskContext()
+  const {state,setState} = useTaskContext()
+
+//Ciclos
+
+const nextCycle = getNextCycle(state.currentCycle)
+console.log(nextCycle)
 
   const handleCreateNewTask = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault() //PARA NÃO ENVIAR O FORM
+    e.preventDefault() 
 
-    //Etapa de segurança, se não tiver nenhum input a função retorna neste ponto e não executa o código  
     if (taskNameInput.current === null) return;
-
-    /////A Partir daqui só executa se existir um input
-
-    //Para resgatar o valor digitado pelo user, trim()para eliminar os espaços desnecessários ' valor '
     const taskName = taskNameInput.current.value.trim()
     console.log(taskName)
 
-    //Se o user não der o nome da task, um alert será disparado na tela e a função no procede
     if (!taskName) {
       alert('Digite o nome da tarefa')
       return
     }
 
-//Quando o user clicar no botão irá criar essa nova Task, com o modelo que já criamos em /model
-//Agora estamos adicionando o nome da task e todos os dados necessários para preencher o TaskModel
     const newTask: TaskModel = {
-      id:Date.now().toString(), //toString para transformar em string//Date.now para pegar a data atual
-      name:taskName, //Adicionando o nome da task através do input que captamos o current.value no hook useRef
+      id:Date.now().toString(), 
+      name:taskName, 
       startDate: Date.now(),
       completeDate:null, //estado
       interruptDate:null,//estado
@@ -41,21 +39,17 @@ export const MainForm = () => {
       type:'workTime',//estado
     }
 
-//aqui estamos formatando o duration da nova task criada em segundos 
 const secondsRemaining = newTask.duration * 60; 
 
-
-//agora temos que configurar o setState 
     setState(prevState =>{
       return{
         ...prevState,
-        config:{...prevState.config}, //apenas para garantir que estamos preenchendo todo o type
-        activeTask: newTask, //Adicionamos a task criada logo a cima em activeTask
-        currentCycle: 1, //conferir
-        secondsRemaining:secondsRemaining, //Aqui como o nome da const secondsRemaining é igual a o elemento do objeto, não precisamos declarar o novo valor.. Aqui deixei o valor declarado para facilitar o entendimento
+        config:{...prevState.config}, 
+        activeTask: newTask, 
+        currentCycle: nextCycle, 
+        secondsRemaining:secondsRemaining, 
         formattedSecondsRemaining: '00:00', //conferir
-      //Arrays são iguais a objetos do state, não podemos muta-lo diretamente, é necessário buscar o valor anterior do array e fazer as alterações com esse valor anterior.. Mesmo que ele seja vazio
-        tasks: [...prevState.tasks,newTask] // aqui estamos colocando a nova task criada no array de tasks
+        tasks: [...prevState.tasks,newTask] 
       }
 
     })
