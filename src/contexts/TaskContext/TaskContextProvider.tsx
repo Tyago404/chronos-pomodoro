8bd116e412f72c1717d5d4e5fdf85377ad1060e7
Tiago Santos <tiago40427@gmail.com>
2025-06-03 22:01:52 -0300

    Adicionando beep ao finalizar task

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
 import { TimerWorkerManager } from '../../workers/timerWorkerManager';
 import { TaskActionTypes } from './taskActions';
+import { loadBeep } from '../../utils/loadBeep';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -12,16 +13,17 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
   const [state, dispatch] = useReducer(taskReducer, initialTaskState)
+  let playBeepRef = useRef<ReturnType<typeof loadBeep>>(null); // resgatando o retorno da função loadbeep
 
-  //para ter acesos ao worker 
   const worker = TimerWorkerManager.getInstance()
   worker.onmessage(e => {
     const countdownSeconds = e.data
-    console.log(countdownSeconds);
-
-
 
     if (countdownSeconds <= 0) {
+      if(playBeepRef.current){
+        playBeepRef.current()
+        playBeepRef.current = null;
+      }
       dispatch({
         type: TaskActionTypes.COMPLETE_TASK,
       });
@@ -34,17 +36,21 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     }
   })
 
-
   useEffect(() => {
     if (!state.activeTask) {
-      console.log('Worker terminado por falta de activeTask');
       worker.terminate()
     }
 
     worker.postMessage(state)
   }, [state, worker])
 
-
+  useEffect(()=>{
+    if(state.activeTask && playBeepRef.current === null){
+      playBeepRef.current = loadBeep()
+    }else{
+         playBeepRef.current = null;
+    }
+  },[state.activeTask])
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
       {children}
