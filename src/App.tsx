5b9432ee142fa053260fcb25250270d8b6753ec3
Tiago Santos <tiago40427@gmail.com>
2025-06-05 23:12:55 -0300

    Criando Componente router e ScrollToTop
    Neste commit fizemos um componente ser nossas rotas e criamos um outro componente <hack> aonde ele volta a tela para o inicio... Fazemos um componente com todas as rotas para melhor manutenção

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
-import { Home } from './pages/Home';
+import { MessagesContainer } from './components/MessagesContainer';
+import { MainRouter } from './router/MainRouter';
 
 import './styles/global.css';
 import './styles/theme.css'
-import { MessagesContainer } from './components/MessagesContainer';
-import { BrowserRouter, Route, Routes } from 'react-router';
-import { NotFound } from './pages/NotFound';
-import { AboutPomodoro } from './pages/AboutPomodoro';
+
 
 export const App = () => {
 
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>)
 };
