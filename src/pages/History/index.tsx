323a6fce411cc34551d4c2353a02e1d2b4a1f169
Tiago Santos <tiago40427@gmail.com>
2025-06-11 23:05:33 -0300

    Adicionando status das tasks no Histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -8,6 +8,7 @@ import { MainTemplate } from '../../templates/MainTemplate';
 import styles from './styles.module.css'
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { formatDate } from '../../utils/formatDate';
+import { getTaskStatus } from '../../utils/getTaskStatus';
 
 export const History = () => {
 
@@ -48,7 +49,7 @@ export const History = () => {
                     <td>{task.name}</td>
                     <td>{task.duration}</td>
                     <td>{formatDate(task.startDate)}</td>
-                    <td>{task.interruptDate}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
                     <td>{task.type}</td>
                   </tr>
                 )
